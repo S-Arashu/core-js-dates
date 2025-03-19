0b5f23d01968822e8cd49317bf2958d0b87b4119
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-03-19 18:40:56 +0300

    feat: task -getNextFridayThe13th should return the date of the next Friday the 13th from a given date- is done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -261,8 +261,20 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const currentDate = new Date(date);
+  let fridayThe13th;
+
+  while (date) {
+    if (currentDate.getDate() === 13 && currentDate.getDay() === 5) {
+      fridayThe13th = currentDate;
+      break;
+    } else {
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+  }
+
+  return fridayThe13th;
 }
 
 /**
